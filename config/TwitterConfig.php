6f4f4836b3591228732dc5f425104b948f020a9d
Rudrakshi <rudrakshi.bargal@gmail.com>
2013-07-15 14:18:09 +0530

    few changes

--- config/TwitterConfig.php
+++ config/TwitterConfig.php
@@ -1,6 +1,13 @@
 <?php
-$twitter=array(
-'consumer_key'    => '',
-      'consumer_secret' => ''
+$apiConfig=array(
+    'client_id'=>'555190471060.apps.googleusercontent.com',
+    'client_secret'=>'FRhRU3F4cnmgMu_CsyIKvkNl',
+    'client_uri'=>'http://localhost/ApprtCamp/',
+   // 'developer_key'=>
+);
+        
+        $twitter=array(
+'consumer_key'    => 'kLytHfRsjICDS5BPRFBFug',
+      'consumer_secret' => 'ic76LkfYQNUovgTEsT7BKc7nlsfadzswt7xBkva64'
     );
 ?>
